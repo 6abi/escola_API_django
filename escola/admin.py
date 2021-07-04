e39d1dec5043b6a5b6d9a016c939044831dd4944
Bárbara <6abi.cardoso@gmail.com>
2021-07-04 00:49:05 -0300

    Criação da Model de Matricula e usando as foreignKey de aluno e curso

--- escola/admin.py
+++ escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 # Register your models here.
 class Alunos(admin.ModelAdmin):
@@ -16,3 +16,9 @@ class Cursos(admin.ModelAdmin):
     search_fields = ('codigo_curso',)
 
 admin.site.register(Curso, Cursos)
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id',)
+
+admin.site.register(Matricula, Matriculas)
