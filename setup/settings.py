4a2b22b9e3c93344b6d76fc036e8bbeb689bca48
Bárbara <6abi.cardoso@gmail.com>
2021-07-08 23:21:56 -0300

    Upload de arquivos estáticos

--- setup/settings.py
+++ setup/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_URL = '/media/'
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
